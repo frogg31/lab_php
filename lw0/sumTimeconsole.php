b2f9d9200233c591eb0e6bedce10f29fcecd158d
frogg31 <113858357+frogg31@users.noreply.github.com>
2022-10-17 22:48:30 +0300

    feat(lw0 sumTime):create sumtime

--- lw0/sumTimeconsole.php
+++ lw0/sumTimeconsole.php
@@ -3,22 +3,19 @@ echo 'ведите первое время: ';
 $argv1 = trim(fgets(STDIN));
 echo 'введите второе время: ';
 $argv2 = trim(fgets(STDIN));
-function sumTime(string $argv1=NULL, string $argv2=null): string
+function sumTime(string $argv1 = NULL, string $argv2 = null): string
 {
 	$colon = [':'];
 	$number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-	$array = str_split($argv1.$argv2);
+	$array = str_split($argv1 . $argv2);
 
-	foreach ($array as $char)
-	{
-		if (!in_array($char, $colon) && !in_array($char, $number))
-		{
-			return('не правильный ввод');
+	foreach ($array as $char) {
+		if (!in_array($char, $colon) && !in_array($char, $number)) {
+			return ('не правильный ввод');
 		}
 	}
 
-    $timeFinal = strtotime($argv1) + strtotime($argv2) ;
-    return date('H:i:s', $timeFinal);
+	$timeFinal = strtotime($argv1) + strtotime($argv2);
+	return date('H:i:s', $timeFinal);
 }
 echo sumTime($argv1, $argv2);
-
