e37362a5b6a7f0475437e3623347968a08beeeba
frogg31 <113858357+frogg31@users.noreply.github.com>
2022-12-06 21:07:29 +0300

    feat(lw1 Calculator): create calculator

---
+++ lw1/calculator.php
@@ -0,0 +1,45 @@
+<?php
+class Calculator
+{
+    public float $calculate;
+
+    public function __construct(float $calculate)
+    {
+        $this->calculate = $calculate;
+    }
+
+    public function sum(float $term): self
+    {
+        $this->calculate = $this->calculate + $term;
+        return $this;
+    }
+
+    public function minus(float $subtrahend): self
+    {
+        $this->calculate = $this->calculate - $subtrahend;
+        return $this;
+    }
+
+    public function product(float $factor): self
+    {
+        $this->calculate = $this->calculate * $factor;
+        return $this;
+    }
+
+    public function division(float $divider): self
+    {
+        if ($divider !== 0) {
+            $this->calculate = $this->calculate / $divider;
+        } else {
+            $this->calculate = 0;
+        }
+        return $this;
+    }
+
+    public function getResult()
+    {
+        return $this->calculate;
+    }
+}
+$calculator = new Calculator(0);
+echo $calculator->sum(5)->product(100)->minus(15)->division(50)->getResult();
