ee46f56ffc83cf2c2a5715a26176a1dba40bee6a
frogg31 <113858357+frogg31@users.noreply.github.com>
2023-03-13 23:27:07 +0300

    feat(lw5 api):create api

--- lw5/src/api.php
+++ lw5/src/api.php
@@ -1,12 +1,14 @@
 <?php
 
-$curl = curl_init('https://favqs.com/api/qotd');
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://favqs.com/api/qotd');
 curl_setopt($curl, CURLOPT_POST, false);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($curl, CURLOPT_HEADER, false);
 $resultCurl = curl_exec($curl);
+$resultCurl = (string) $resultCurl;
 $resultCurl = json_decode($resultCurl);
 $result = $resultCurl->quote->body;
 echo $result;
-curl_close($curl);   
+curl_close($curl);
